d7f625d8bd0728ec2d9641b7ce9c7824636114ac
Sam Perozek <samperozek@gmail.com>
2022-03-24 09:39:13 -0600

    commit tree 47894449e2108ddd12fcf5e798b3f32368b5f6b6

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+let copyCats = [];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+};
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+};
+function appendCat(name){
+    return copyCats = [...cats,name];
+};
+function prependCat(name){
+    return copyCats = [name,...cats];
+};
+function removeLastCat(name){
+    return copyCats = cats.slice(0,cats.length-1);
+};
+function removeFirstCat(name){
+    return copyCats = cats.slice(1,cats.length);
+};
